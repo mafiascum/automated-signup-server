c458c1b555eeff4e0b1e6e06204e6cda50191391
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-25 02:29:27 -0500

    Ability spec

---
+++ spec/models/user_ability_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'cancan/matchers'
+
+RSpec.describe User, type: :model do
+  subject(:ability) do
+    Ability.new(user)
+  end
+
+  context 'as an admin' do
+    let(:user) do
+      FactoryGirl.create(:user, role: :admin)
+    end
+
+    it 'can manage users' do
+      is_expected.to be_able_to(:manage, User.new)
+    end
+  end
+
+  context 'as a player' do
+    let(:user) do
+      FactoryGirl.create(:user, role: :player)
+    end
+
+    it 'can read users' do
+      is_expected.to be_able_to(:read, User.new)
+    end
+
+    it 'can reset their own token' do
+      is_expected.to be_able_to(:reset_token, user)
+    end
+
+    it 'cannot reset tokens that are not their own' do
+      is_expected.not_to be_able_to(:reset_token, User.new)
+    end
+  end
+end
