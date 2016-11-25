7a0f7762bda37758319e98af4f6cf88a2139ee15
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-25 02:30:34 -0500

    Add authenticaton/authorization to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # Base controller class
 class ApplicationController < ActionController::API
+  respond_to :json
+
   include CanCan::ControllerAdditions
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+  check_authorization
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token|
+      @current_user = User.find_by(token: token)
+    end
+  end
 end
