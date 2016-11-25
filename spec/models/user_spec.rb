c36f12990490ef86d5e783c66cdb510fe0b68f6d
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-25 00:31:54 -0500

    Add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  it 'is invalid wihout a role' do
+    expect(FactoryGirl.build(:user, role: nil)).not_to be_valid
+  end
 end
