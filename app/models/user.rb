fd6da8c02ce8cfd319d288447b7473216c4ea8c7
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-25 00:32:11 -0500

    Add user model content

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# Represents forum users in the rails database
+# User.id should be the user's forum id
 class User < ApplicationRecord
+  enum role: [:player, :listmod, :admin]
+  has_secure_token :token
+
+  validates :role, presence: true
 end
