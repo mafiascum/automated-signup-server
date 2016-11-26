ed88325bb0eba295550fc7ea54192af4e26bdee6
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-26 12:26:48 -0500

    Add GameQueue spec

--- spec/models/game_queue_spec.rb
+++ spec/models/game_queue_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe GameQueue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(FactoryGirl.build(:game_queue)).to be_valid
+  end
+
+  it 'is invalid wihout a name' do
+    expect(FactoryGirl.build(:game_queue, name: nil)).not_to be_valid
+  end
+
+  it 'is invalid with a name under 3 characters' do
+    expect(FactoryGirl.build(:game_queue, name: 'aa')).not_to be_valid
+  end
+
+  it 'is invalid with a name over 64 characters' do
+    expect(FactoryGirl.build(:game_queue, name: 'a' * 65)).not_to be_valid
+  end
+
+  it 'is invalid without a forum id' do
+    expect(FactoryGirl.build(:game_queue, forum_id: nil)).not_to be_valid
+  end
+
+  it 'is invalid with a duplicate forum id' do
+    FactoryGirl.create(:game_queue, forum_id: 1)
+    expect(FactoryGirl.build(:game_queue, forum_id: 1)).not_to be_valid
+  end
 end
