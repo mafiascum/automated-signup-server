d394d0250217bbc6b7b3a9a506b200faff667968
Connor Catlett <ccatlett2000@mctherealm.net>
2016-11-26 12:27:00 -0500

    Add GameQueue model

--- app/models/game_queue.rb
+++ app/models/game_queue.rb
@@ -1,2 +1,5 @@
+# Games are sorted by characteristics into queues
 class GameQueue < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 3, maximum: 64}
+  validates :forum_id, presence: true, uniqueness: true
 end
